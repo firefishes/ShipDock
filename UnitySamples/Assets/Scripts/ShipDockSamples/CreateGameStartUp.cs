439898a9c55ea2a66b7250f6ca05365786e122de
firefishes <279034231@qq.com>
2021-07-15 23:39:01 +0800

    更新‘框架新建应用’的案例场景

---
+++ UnitySamples/Assets/Scripts/ShipDockSamples/CreateGameStartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ShipDock.Applications;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreateGameStartUp : ShipDockAppComponent
+{
+    protected override string GetLocalsDescription<T>(ref string locals, ref T item)
+    {
+        return string.Empty;
+    }
+
+    //public override void ApplicationCloseHandler()
+    //{
+    //    base.ApplicationCloseHandler();
+
+    //    Scene scene = SceneManager.CreateScene("Empty");
+    //    SceneManager.SetActiveScene(scene);
+    //    SceneManager.MergeScenes(SceneManager.GetActiveScene(), scene);
+    //    GameObject[] list = scene.GetRootGameObjects();
+    //    int max = list.Length;
+    //    for (int i = 0; i < max; i++)
+    //    {
+    //        Destroy(list[i]);
+    //    }
+
+    //    SceneManager.sceneUnloaded += UnloadEmptyScene;
+    //    SceneManager.UnloadSceneAsync(scene);
+    //}
+
+    //private void UnloadEmptyScene(Scene scene)
+    //{
+    //    SceneManager.sceneUnloaded -= UnloadEmptyScene;
+
+    //    if (Application.isPlaying)
+    //    {
+    //        SceneManager.sceneLoaded += OnReloadStartUpScene;
+    //        SceneManager.LoadScene("Sample_CreateGameStartUp");
+    //    }
+    //    else { }
+    //}
+
+    //private void OnReloadStartUpScene(Scene scene, LoadSceneMode mode)
+    //{
+    //    SceneManager.sceneLoaded -= OnReloadStartUpScene;
+    //    SceneManager.SetActiveScene(scene);
+    //}
+
+    public override void EnterGameHandler()
+    {
+        base.EnterGameHandler();
+
+        GameObject raw = ShipDockApp.Instance.ABs.Get("aa", "a");
+        Instantiate(raw);
+
+        TimeUpdater.New(5f, () =>
+        {
+            ReloadFrameworkScene();
+        });
+    }
+}
