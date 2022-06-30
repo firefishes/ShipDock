d545ac94ace6c8ff599021ae49e26993bd5afd20
firefishes <279034231@qq.com>
2022-07-01 02:52:49 +0800

    测试加载配置并解析配置功能

--- UnitySamples/Assets/Scripts/PeaceGame/Consts.cs
+++ UnitySamples/Assets/Scripts/PeaceGame/Consts.cs
@@ -1,6 +1,6 @@
-using System.Collections;
+using ShipDock.Applications;
+using ShipDock.Config;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Peace
 {
@@ -23,6 +23,19 @@ namespace Peace
 
         public const int MSG_ENTER_BATTLE = 20000;
 
+        public const int CONF_GROUP_CONFIGS = 1;
 
+        public const string CONF_EQUIPMENT = "peaceequipment";
+
+        public const string AB_CONFIGS = "peace/configs";
+
+        /// <summary>游戏配置案例中获取配置的扩展方法</summary>
+        public static Dictionary<int, ConfigT> GetConfigTable<ConfigT>(this string configName) where ConfigT : IConfig, new()
+        {
+            ConfigData data = D_CONFIGS.GetData<ConfigData>();
+            ConfigsResult configs = data.GetConfigs(CONF_GROUP_CONFIGS);
+            Dictionary<int, ConfigT> dic = configs.GetConfigRaw<ConfigT>(configName, out _);
+            return dic;
+        }
     }
 }
--- UnitySamples/Assets/Scripts/PeaceGame/PeaceStartUp.cs
+++ UnitySamples/Assets/Scripts/PeaceGame/PeaceStartUp.cs
@@ -3,6 +3,7 @@ using ShipDock.Datas;
 using ShipDock.Modulars;
 using ShipDock.Notices;
 using ShipDock.Pooling;
+using StaticConfig;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,9 @@ namespace Peace
 {
     public class PeaceStartUp : ShipDockAppComponent
     {
+        private ConfigHelper mConfigHelper;
+        private List<string> mLoadConfigNames;
+
         public override void ApplicationCloseHandler()
         {
             base.ApplicationCloseHandler();
@@ -42,12 +46,51 @@ namespace Peace
             DecorativeModulars modluars = ShipDockApp.Instance.AppModulars;
             modluars.AddModular(modulars);
 
-            IParamNotice<string> notice = new ParamNotice<string>()
+            //IParamNotice<string> notice = new ParamNotice<string>()
+            //{
+            //    ParamValue = "！！！",
+            //};
+
+            //modluars.NotifyModular(Consts.N_MSG_ADD, MessageNotice.Create(Consts.MSG_ENTER_BATTLE, notice));
+
+            LoadConfig();
+        }
+
+        private void LoadConfig()
+        {
+            //新建配置辅助器
+            mConfigHelper = new ConfigHelper
             {
-                ParamValue = "！！！",
+                //设置配置文件所在的资源包名
+                ConfigResABName = Consts.AB_CONFIGS,
             };
 
-            modluars.NotifyModular(Consts.N_MSG_ADD, MessageNotice.Create(Consts.MSG_ENTER_BATTLE, notice));
+            //向配置辅助器添加需要绑定的配置类，此类为工具自动生成
+            mConfigHelper.AddHolderType<PeaceEquipment>(Consts.CONF_EQUIPMENT);
+            //设置配置名
+            mLoadConfigNames = mConfigHelper.HolderTypes;
+
+            //根据配置名加载配置
+            string[] list = mLoadConfigNames.ToArray();
+            mConfigHelper.Load(OnConfigLoaded, list);
+        }
+
+        //配置加载完成
+        private void OnConfigLoaded(ConfigsResult configResult)
+        {
+            ConfigData data = Consts.D_CONFIGS.GetData<ConfigData>();
+
+            //向配置数据代理中添加已加载的配置数据，并使用一个组名做对应
+            data.AddConfigs(Consts.CONF_GROUP_CONFIGS, configResult);
+
+            //通过封装过的扩展方法从配置数据代理中获取配置
+            Dictionary<int, PeaceEquipment> equipmentConfTable = Consts.CONF_EQUIPMENT.GetConfigTable<PeaceEquipment>();
+
+            //从配置中读取数据
+            int id = 20000;
+            PeaceEquipment config = equipmentConfTable[id];
+            Debug.Log(config.name);
+            Debug.Log(config.propertyValues);
         }
     }
 }
